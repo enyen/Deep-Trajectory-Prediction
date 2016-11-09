4d803c463ae26bfa6645f44fddff297857df34d5
enyen <puangenyen@gmail.com>
2016-11-09 20:35:34 +0100

    import graph to ros env

--- nn_trajectory/src/nn_predict.py
+++ nn_trajectory/src/nn_predict.py
@@ -6,22 +6,26 @@ from __future__ import print_function
 
 import sys
 import rospy
-from nav_msgs.msg import _Odometry
-from nav_msgs.msg import _Path
+from nav_msgs.msg import *
+from geometry_msgs.msg import *
 
 import numpy as np
 import tensorflow as tf
 
+traj_back = 20
 
-class nn_predict:
+
+class nnPredict:
+    path_pass = []
+
     def __init__(self):
-        self.pub_path_pass = rospy.Publisher('/human_pose/path_pass_nn', _Path.Path, queue_size=1)
-        self.pub_path_predict = rospy.Publisher('/human_pose/path_future_nn', _Path.Path, queue_size=1)
+        self.pub_path_pass = rospy.Publisher('/human_pose/path_pass_nn', Path, queue_size=1)
+        self.pub_path_predict = rospy.Publisher('/human_pose/path_future_nn', Path, queue_size=1)
         odom_topic = rospy.get_param('~odom_topic', '')
         if odom_topic == '':
             rospy.logerr('No odom_topic provided, check launch file.')
             sys.exit()
-        rospy.Subscriber('/bill', _Odometry.Odometry, self.handle_odom)
+        rospy.Subscriber('/bill', Odometry, self.handle_odom)
 
         model_file = rospy.get_param('~model_file', '')
         model_meta_file = rospy.get_param('~model_meta_file', '')
@@ -33,15 +37,30 @@ class nn_predict:
         saver = tf.train.import_meta_graph(model_meta_file)
         saver.restore(self.sess, model_file)
         self.predict = tf.get_collection('y3')[0]
+        self.input = tf.get_collection('x1')[0]
+        self.drop = tf.get_collection('keep_prob')[0]
 
-    def handle_odom(self, data):
-        # print (self.predict.eval(feed_dict={x1: train_x, keep_prob: 1.0}))
-        print(self.predict.eval(feed_dict={x1: batch_xs, keep_prob: 0.5}, session=self.sess))
+    def handle_odom(self, msg):
+        if len(self.path_pass) >= traj_back:
+            self.path_pass.pop(0)
+
+        self.path_pass.append(PoseStamped(header=msg.header, pose=msg.pose.pose))
+
+        if len(self.path_pass) == traj_back:
+            self.pub_path_pass.publish(Path(header=msg.header, poses=self.path_pass))
+            batch_xs = np.zeros([traj_back, 3])
+            for i in range(0, traj_back):
+                batch_xs[i, 0] = (self.path_pass[i].header.stamp - self.path_pass[0].header.stamp).to_sec()
+                batch_xs[i, 1] = self.path_pass[i].pose.position.x
+                batch_xs[i, 2] = self.path_pass[i].pose.position.y
+            batch_xs = batch_xs - np.mean(batch_xs, axis=0)
+            batch_xs = batch_xs / np.std(batch_xs, axis=0)
+            print(self.predict.eval(feed_dict={self.input: np.reshape(batch_xs, newshape=[1,traj_back*3]), self.drop: 0.5}, session=self.sess))
 
 
 def main():
     rospy.init_node('nn_predict', anonymous=True)
-    nn_predict()
+    nnPredict()
     rospy.spin()
 
 if __name__ == '__main__':
