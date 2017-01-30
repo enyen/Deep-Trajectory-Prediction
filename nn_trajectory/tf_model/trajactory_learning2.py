e191e885b0f8343a5a3a34523e8a26d45f8d0476
enyen <puangenyen@gmail.com>
2017-01-30 03:55:29 +0100

    model update

--- nn_trajectory/tf_model/trajactory_learning2.py
+++ nn_trajectory/tf_model/trajactory_learning2.py
@@ -5,30 +5,38 @@ from __future__ import print_function
 # Import data
 import trajactory_data as input_data
 import tensorflow as tf
+from tensorflow.contrib import layers
 
 # ((W - F + 2P) / S) + 1
 scan_size = 173
 traj_back, traj_front, order = 30, 30, 2
-h1_len, h2_len, h3_len, h4_len, h5_len = 64, 128, 256, 512, 1024
+h1_len, h2_len, h3_len = 64, 128, 256
 out_len = traj_front * 2
 
 savedModel = 'saved_scan2/model'
-restoreModel = True
-minibatch, learnrate, dropout, iteration = 128, 1e-5, 0.5, 60000
+restoreModel = False
+minibatch, learnrate_, alpha_lrelu, dropout, iteration = 64, 1e-3, 0.01, 0.5, 60000
 regenerateData = False
 
 
 def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
+    initial = tf.truncated_normal(shape, stddev=0.05)
     return tf.Variable(initial)
 
 
 def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
+    initial = tf.constant(0.05, shape=shape)
     return tf.Variable(initial)
 
 
+def leakyRelu(x, alpha):
+    x_norm = layers.batch_norm(x)
+    return tf.maximum(alpha*x_norm, x_norm)
+
+
 def main(_):
+    learnrate = learnrate_
+
     # produce data
     if regenerateData:
         input_data.produce_data_scan1(traj_back, traj_front, order)
@@ -45,38 +53,28 @@ def main(_):
     # layer 1 conv
     w1 = weight_variable([1, 5, traj_back, h1_len])
     b1 = bias_variable([h1_len])
-    x2 = tf.nn.relu(tf.nn.conv2d(x1, w1, strides=[1, 2, 2, 1], padding='VALID', use_cudnn_on_gpu=True) + b1)
-    # x2_ = tf.nn.relu(tf.nn.conv2d(x1, w1, strides=[1, 1, 1, 1], padding='VALID', use_cudnn_on_gpu=True) + b1)
-    # x2 = tf.nn.max_pool(x2_, ksize=[1, 1, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    x2 = leakyRelu(tf.nn.conv2d(x1, w1, strides=[1, 3, 3, 1], padding='VALID', use_cudnn_on_gpu=False) + b1, alpha_lrelu)
 
     # layer 2 conv
-    w2 = weight_variable([1, 5, h1_len, h2_len])
+    w2 = weight_variable([1, 3, h1_len, h2_len])
     b2 = bias_variable([h2_len])
-    x3 = tf.nn.relu(tf.nn.conv2d(x2, w2, strides=[1, 2, 2, 1], padding='VALID', use_cudnn_on_gpu=True) + b2)
-    # x3_ = tf.nn.relu(tf.nn.conv2d(x2, w2, strides=[1, 1, 1, 1], padding='VALID', use_cudnn_on_gpu=True) + b2)
-    # x3 = tf.nn.max_pool(x3_, ksize=[1, 1, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    x3_ = leakyRelu(tf.nn.conv2d(x2, w2, strides=[1, 2, 2, 1], padding='VALID', use_cudnn_on_gpu=False) + b2, alpha_lrelu)
+    x3 = tf.nn.dropout(x3_, keep_prob=keep_prob)
 
-    # layer 3 conv
-    w3 = weight_variable([1, 3, h2_len, h3_len])
+    # layer 3 fc
+    w3 = weight_variable([28 * h2_len, h3_len])
     b3 = bias_variable([h3_len])
-    x4 = tf.nn.relu(tf.nn.conv2d(x3, w3, strides=[1, 2, 2, 1], padding='VALID', use_cudnn_on_gpu=True) + b3)
-    # x4_ = tf.nn.relu(tf.nn.conv2d(x3, w3, strides=[1, 1, 1, 1], padding='VALID', use_cudnn_on_gpu=True) + b3)
-    # x4 = tf.nn.max_pool(x4_, ksize=[1, 1, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    x4_ = leakyRelu(tf.matmul(tf.reshape(x3, [-1, 28 * h2_len]), w3) + b3, alpha_lrelu)
+    xl = tf.nn.dropout(x4_, keep_prob=keep_prob)
 
     # layer 4 fc
-    w4 = weight_variable([20 * h3_len, h4_len])
-    b4 = bias_variable([h4_len])
-    x5_ = tf.nn.relu(tf.matmul(tf.reshape(x4, [-1, 20 * h3_len]), w4) + b4)
-    x5 = tf.nn.dropout(x5_, keep_prob=keep_prob)
-
-    # layer 5 fc
-    w5 = weight_variable([h4_len, h5_len])
-    b5 = bias_variable([h5_len])
-    xl = tf.nn.tanh(tf.matmul(x5, w5) + b5)
+    # w7 = weight_variable([h6_len, h7_len])
+    # b7 = bias_variable([h7_len])
+    # xl_ = leakyRelu(tf.matmul(x7, w7) + b7, alpha_lrelu)
     # xl = tf.nn.dropout(xl_, keep_prob=keep_prob)
 
     # layer output fc
-    wl = weight_variable([h5_len, out_len])
+    wl = weight_variable([h3_len, out_len])
     bl = bias_variable([out_len])
     yl = tf.matmul(xl, wl) + bl
 
@@ -99,7 +97,8 @@ def main(_):
     if restoreModel:
         saver.restore(sess, savedModel)
     else:
-        tf.initialize_all_variables().run()
+        tf.global_variables_initializer().run()
+        # tf.initialize_all_variables().run()
     tf.add_to_collection("yl", yl)
     tf.add_to_collection("x1", x1)
     tf.add_to_collection("keep_prob", keep_prob)
@@ -116,15 +115,16 @@ def main(_):
         train_step.run(feed_dict={x1: batch_xs, y_: batch_ys, keep_prob: dropout, learn_rate: learnrate})
 
         # performance evaluation
-        if i % 20 == 0 and i != trajactory.train.epochs_completed:
+        if i % 2 == 0 and i != trajactory.train.epochs_completed:
             train_loss = predict_loss.eval(feed_dict={x1: batch_xs, y_: batch_ys, keep_prob: 1.0})
             test_x, test_y = trajactory.test.next_batch(minibatch)
             test_loss = predict_loss.eval(feed_dict={x1: test_x, y_: test_y, keep_prob: 1.0})
             print("epoch %d, t_loss %g, v_loss %g" % (i, train_loss, test_loss))
 
         # variables storing
-        if (i % 1000 == 0) and (i != 0) and i != trajactory.train.epochs_completed:
+        if (i % 100 == 0) and (i != 0) and i != trajactory.train.epochs_completed:
             saver.save(sess, savedModel)
+            learnrate /= 1.5
             print("model saved.")
 
         i = trajactory.train.epochs_completed
